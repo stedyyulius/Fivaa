3c3f05c5b6c3be429e9d11ffa0b800f05c116209
stedyyulius <stedyyulius@gmail.com>
2018-10-27 14:17:53 +0700

    done

---
+++ Fivaa.js
@@ -0,0 +1,14 @@
+const fivaa = (amount) => {
+  const leftNumbers = amount - 1;
+  const rightNumbers = amount + 1;
+  let str = leftNumbers.toString() + leftNumbers.toString();
+
+  if (amount > 0) {
+    for (let i = 0; i < amount; i++) {
+      str += rightNumbers.toString()
+    }
+    return fivaa(amount - 1);
+  }
+}
+
+fivaa(5);
